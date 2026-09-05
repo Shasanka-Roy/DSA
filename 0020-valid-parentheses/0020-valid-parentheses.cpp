0e13f241404d06528769390eec1ae044fcd1a21e
Shasanka Roy <shasankaroy295@gmail.com>
2026-09-05 12:55:35 -0700

    Time: 0 ms (100%), Space: 8.9 MB (67.28%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> st;
+
+        for (char c : s) {
+
+            // Opening brackets
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            }
+
+            // Closing brackets
+            else {
+
+                if (st.empty())
+                    return false;
+
+                char top = st.top();
+
+                if (c == ')' && top != '(')
+                    return false;
+
+                if (c == ']' && top != '[')
+                    return false;
+
+                if (c == '}' && top != '{')
+                    return false;
+
+                st.pop();
+            }
+        }
+
+        // Stack should be empty
+        return st.empty();
+    }
+};
